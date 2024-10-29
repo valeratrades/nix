f95b33b8dc98fd057024b127f07a371f185b1d80
valeratrades <v79166789533@gmail.com>
2024-10-29 09:11:48 +0000

    _

--- os/configuration.nix
+++ os/configuration.nix
@@ -95,15 +95,15 @@ in
   };
 
   # Set your time zone.
-  time.timeZone = "Europe/Paris";
+  time.timeZone = "UTC";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "fr_FR.UTF-8";
     LC_IDENTIFICATION = "fr_FR.UTF-8";
-    LC_MEASUREMENT = "fr_FR.UTF-8";
-    LC_MONETARY = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
     LC_NAME = "fr_FR.UTF-8";
     LC_NUMERIC = "fr_FR.UTF-8";
     LC_PAPER = "fr_FR.UTF-8";
