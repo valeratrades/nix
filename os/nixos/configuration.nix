0260611ca25daa89adc7b52e8a61bf0a9e421908
valeratrades <v79166789533@gmail.com>
2024-12-27 16:00:15 +0000

    fix: _

--- os/nixos/configuration.nix
+++ os/nixos/configuration.nix
@@ -1061,7 +1061,7 @@ in
         # Coding
         [
           vscode-extensions.github.copilot
-          mold
+          mald-wrapped # probably shouldn't be here though
           sccache
           just
           toml-cli
