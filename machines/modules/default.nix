837a45dde80e66cfe7726a7a935ec1f48c03ff6b
valeratrades <valeratrades@gmail.com>
2025-01-23 17:23:52 +0000

    _

--- machines/modules/default.nix
+++ machines/modules/default.nix
@@ -5,10 +5,15 @@
   ...
 }:
 {
-  security.sudo.execWheelOnly = lib.mkForce false;
+  security.sudo = builtins.trace "DEBUG: loading `machines` module" {
+    execWheelOnly = lib.mkForce false;
+    # allow for running sudo when connected via ssh
+    extraConfig = ''
+      	Defaults env_keep += "SSH_AUTH_SOCK"
+      		'';
+  };
 
   imports = [
     ./fhs-compat.nix
   ];
 }
-
