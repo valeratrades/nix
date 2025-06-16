58abedba2b16e3d014a30234a733ca804d16e3e7
Valera <valeratrades@gmail.com>
2025-06-16 18:20:34 +0000

    _

--- os/nixos/configuration.nix
+++ os/nixos/configuration.nix
@@ -128,6 +128,8 @@ in {
         "	export XDG_CURRENT_DESKTOP=\"sway\";\n	export GDK_BACKEND=\"wayland\";\n	export XDG_BACKEND=\"wayland\";\n	export QT_WAYLAND_FORCE_DPI=\"physical\";\n	export QT_QPA_PLATFORM=\"wayland-egl\";\n	export CLUTTER_BACKEND=\"wayland\";\n	export SDL_VIDEODRIVER=\"wayland\";\n	export BEMENU_BACKEND=\"wayland\";\n	export MOZ_ENABLE_WAYLAND=\"1\";\n	# QT (needs qt5.qtwayland in systemPackages)\n	export QT_QPA_PLATFORM=wayland-egl\n	export SDL_VIDEODRIVER=wayland\n";
     };
     sway.xwayland.enable = true;
+    fish.enable =
+      true; # HACK: things break if I remove it here. However, `hm` does `fish.enable` already. Conundrum.
 
     # conflicts with gnupg agent on which I have ssh support. TODO: figure out which one I want
     ssh = {
