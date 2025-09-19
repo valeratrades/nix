d652b960b26ca9fa5c62726f400586c8cb4272da
Valera <valeratrades@gmail.com>
2025-09-19 19:30:19 +0000

    fixup! fixup! _

--- hosts/hm-shared/home.nix
+++ hosts/hm-shared/home.nix
@@ -343,22 +343,10 @@
 				source = "${self}/home/config/mako";
 				recursive = true;
 			};
-		}
-			// pkgs.lib.optionalAttrs (user.kbd != "ansi") (
-				let
-					cfgPath  = "${self}/home/config/sway/config";
-					config   = builtins.readFile cfgPath;
-					needle   = ''xkb_variant "iso,"'';
-					replaced =
-						if builtins.match ".*xkb_variant \"ansi,\".*" config != null
-							then builtins.replaceStrings [ needle ] [ ''xkb_variant "iso,"'' ] config
-						else throw "pattern '${needle}' not found in ${cfgPath}";
-				in {
-					".config/sway/config".source = pkgs.writeText "sway_conf_for_iso_kbd" replaced;
-				}
-			)
-			// pkgs.lib.optionalAttrs (user.kbd == "ansi") {
-				".config/sway" = { source = "${self}/home/config/sway"; recursive = true; };
+			".config/wireplumber" = {
+				source = "${self}/home/config/wireplumber";
+				recursive = true;
 			};
+		};
 	};
 }
