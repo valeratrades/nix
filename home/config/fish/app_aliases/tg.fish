217e936acaf73944a83d61741ee90650a7e4f6eb
Valera <valeratrades@gmail.com>
2025-12-26 23:33:06 +0000

    _

--- home/config/fish/app_aliases/tg.fish
+++ home/config/fish/app_aliases/tg.fish
@@ -1,8 +1,8 @@
-function tgj; tg send journal  $argv >/dev/null; end
-function tgl; tg send learning $argv >/dev/null; end
-function tgm; tg send math     $argv >/dev/null; end
-function tgp; tg send papers   $argv >/dev/null; end
-function tgt; tg send tooling  $argv >/dev/null; end
-function tgr; tg send trading  $argv >/dev/null; end
-function tgv; tg send videos   $argv >/dev/null; end
-function tgw; tg send work     $argv >/dev/null; end
+function tgj; tg send journal  $argv 2>/dev/null; end
+function tgl; tg send learning $argv 2>/dev/null; end
+function tgm; tg send math     $argv 2>/dev/null; end
+function tgp; tg send papers   $argv 2>/dev/null; end
+function tgt; tg send tooling  $argv 2>/dev/null; end
+function tgr; tg send trading  $argv 2>/dev/null; end
+function tgv; tg send videos   $argv 2>/dev/null; end
+function tgw; tg send work     $argv 2>/dev/null; end
