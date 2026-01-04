67e13f60dc275db43f6be122d444759c9e72d310
Valera <valeratrades@gmail.com>
2026-01-04 17:22:39 +0000

    fix: eww: layout: dynamic detection

--- home/config/eww/modules/layout.sh
+++ home/config/eww/modules/layout.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 # default layout is semimak. If you wrote something in russian, you are expected to instantly switch back to semimak. Similar to `normal` mode in vim, - want to be able to assume at any time that current layout is semimak.
 
-index=$(swaymsg -t get_inputs | grep "xkb_active_layout_index" | head -1 | awk '{print $2}' | tr -d ',')
+# Get the active layout name directly from swaymsg
+current_layout=$(swaymsg -t get_inputs --raw | jq -r '[.[] | select(.xkb_active_layout_name != null) | .xkb_active_layout_name][0]')
 
-layouts=$(localectl status | grep "X11 Layout" | awk '{print $3}')
-
-line_number=$(( index + 1 ))
-current_layout=$(echo "$layouts" | tr ',' '\n' | sed -n "${line_number}p")
-
-if [ "$current_layout" = "semimak" ]; then
-  echo ""
-else
-  echo "$current_layout"
-fi
+# Normalize: semimak variants (ANSI, ISO, etc.) should all be treated as semimak
+case "$current_layout" in
+  semimak*|Semimak*)
+    echo ""
+    ;;
+  *)
+    echo "$current_layout"
+    ;;
+esac
